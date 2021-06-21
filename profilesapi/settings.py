6a32149386e6b784055ed2e179d6cbd126746f3d
tanvi <tkp190001@utdallas.edu>
2021-06-21 07:44:34 -0500

    v2

--- profilesapi/settings.py
+++ profilesapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x6o#3!@%8-cqowx8($6gsiclu7!6i)wlb%z6vr-z+$@*8wr2x='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
